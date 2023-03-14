c677d559b5e20bf7e6bb88b2bb6bbf32a7268dad
Abduganiyev <mr.show050@gmail.com>
2023-03-14 21:55:41 +0500

    init

---
+++ src/main/java/com/nuu/design_home/DesignHomeApplication.java
@@ -0,0 +1,13 @@
+package com.nuu.design_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuu/design_home/DesignHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuu.design_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
